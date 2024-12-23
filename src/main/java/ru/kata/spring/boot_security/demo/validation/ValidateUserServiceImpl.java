0cb83e52f9a588c1a07d882bc54895c315e91252
xeplex <umnikov1337@gmail.com>
2024-12-23 16:32:36 +0300

    фикс ошибки отображения всех пользователей после ошибки валидации

--- src/main/java/ru/kata/spring/boot_security/demo/validation/ValidateUserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/validation/ValidateUserServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.validation;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
@@ -8,7 +9,7 @@ import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.util.List;
-
+@Transactional
 @Service
 public class ValidateUserServiceImpl implements ValidateUserService {
 
@@ -26,13 +27,7 @@ public class ValidateUserServiceImpl implements ValidateUserService {
             model.addAttribute("errorMessage",
                     "Пользователь с таким именем уже существует.");
             model.addAttribute("activeTab", "newUser");
-            List<User> users = userService.getAll();
-            for (User userAfterError : users) {
-                userAfterError.setRoles(roleService.findRolesByUserId(userAfterError.getId()));
-            }
-            List<Role> roles = roleService.getAll();
-            model.addAttribute("allRoles", roles);
-            model.addAttribute("users", users);
+            showUsersAndRolesAfterAction(model);
             return true;
         }
         return false;
@@ -43,14 +38,8 @@ public class ValidateUserServiceImpl implements ValidateUserService {
         if (userService.existsByEmail(user.getEmail())) {
             model.addAttribute("errorMessage",
                     "Пользователь с таким адресом электронной почты уже существует.");
-            model.addAttribute("activeTab", "newUser ");
-            List<User> users = userService.getAll();
-            for (User userAfterError : users) {
-                userAfterError.setRoles(roleService.findRolesByUserId(userAfterError.getId()));
-            }
-            List<Role> roles = roleService.getAll();
-            model.addAttribute("allRoles", roles);
-            model.addAttribute("users", users);
+            model.addAttribute("activeTab", "newUser");
+            showUsersAndRolesAfterAction(model);
             return true;
         }
         return false;
@@ -61,12 +50,14 @@ public class ValidateUserServiceImpl implements ValidateUserService {
         if (userService.existsByUsername(user.getUsername()) &&
                 !userService.findByUsername(user.getUsername()).getId().equals(id)) {
             model.addAttribute("errorMessage", "Пользователь с таким именем уже существует.");
+            showUsersAndRolesAfterAction(model);
             return true;
         }
         if (userService.existsByEmail(user.getEmail()) &&
                 !userService.findByEmail(user.getEmail()).getId().equals(id)) {
             model.addAttribute("errorMessage",
                     "Пользователь с таким адресом электронной почты уже существует.");
+            showUsersAndRolesAfterAction(model);
             return true;
         }
         return false;
@@ -76,16 +67,20 @@ public class ValidateUserServiceImpl implements ValidateUserService {
         if (user.getAge() < 1 || user.getAge() > 100) {
             model.addAttribute("errorMessage",
                     "Неверный возраст - возраст должен быть от 1 года до 100 лет");
-            List<User> users = userService.getAll();
-            for (User updated : users) {
-                updated.setRoles(roleService.findRolesByUserId(updated.getId()));
-            }
-            List<Role> roles = roleService.getAll();
-            model.addAttribute("allRoles", roles);
-            model.addAttribute("users", users);
+            showUsersAndRolesAfterAction(model);
             return true;
         }
         return false;
+    }
+
+    private void showUsersAndRolesAfterAction(Model model) {
+        List<User> users = userService.getAll();
+        for (User userAfterError : users) {
+            userAfterError.setRoles(roleService.findRolesByUserId(userAfterError.getId()));
+        }
+        List<Role> roles = roleService.getAll();
+        model.addAttribute("allRoles", roles);
+        model.addAttribute("users", users);
     }
 
 }
